a6336201b5777b540605784447863e57364f00c8
mulin <mulin@tencent.com>
2021-01-28 13:01:16 +0800

    HostToContainer

--- build-container.sh
+++ build-container.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-LXCFS_VERSION=3.1.2
-docker build -t csighub.tencentyun.com/mulin/lxcfs:${LXCFS_VERSION} lxcfs-image/dockerfile.${LXCFS_VERSION} .
-docker push csighub.tencentyun.com/mulin/lxcfs:${LXCFS_VERSION}
+LXCFS_VERSION=latest
+#docker build -t csighub.tencentyun.com/mulin/lxcfs:${LXCFS_VERSION} -f lxcfs-image/dockerfile.${LXCFS_VERSION} .
+#docker push csighub.tencentyun.com/mulin/lxcfs:${LXCFS_VERSION}
+
+docker build -t ccr.ccs.tencentyun.com/mulin-lxcfs/lxcfs:${LXCFS_VERSION} -f lxcfs-image/dockerfile.ccr.${LXCFS_VERSION} .
+docker push ccr.ccs.tencentyun.com/mulin-lxcfs/lxcfs:${LXCFS_VERSION}
 
 ./build
