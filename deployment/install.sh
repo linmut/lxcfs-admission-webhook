707af89cfcc9135d86e63214b34569451f7e45af
mulin <mulin@tencent.com>
2021-02-03 13:08:59 +0800

    for tkex

--- deployment/install.sh
+++ deployment/install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+LIST_TYPE="balcklist"
+#LIST_TYPE="whitelist"
+
 kubectl create ns lxcfs
 ./deployment/webhook-create-signed-cert.sh --namespace lxcfs
 kubectl -n lxcfs get secret lxcfs-admission-webhook-certs
 
 kubectl -n lxcfs create -f deployment/deployment.yaml
 kubectl -n lxcfs create -f deployment/service.yaml
-cat ./deployment/mutatingwebhook.yaml | ./deployment/webhook-patch-ca-bundle.sh > ./deployment/mutatingwebhook-ca-bundle.yaml
-kubectl -n lxcfs create -f deployment/mutatingwebhook-ca-bundle.yaml
+cat ./deployment/mutatingwebhook-${LIST_TYPE}.yaml | ./deployment/webhook-patch-ca-bundle.sh > ./deployment/mutatingwebhook-ca-bundle-${LIST_TYPE}.yaml
+kubectl -n lxcfs create -f deployment/mutatingwebhook-ca-bundle-${LIST_TYPE}.yaml
 
--- deployment/uninstall.sh
+++ deployment/uninstall.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+LIST_TYPE="balcklist"
+#LIST_TYPE="whitelist"
 
-kubectl -n lxcfs delete -f deployment/mutatingwebhook-ca-bundle.yaml
+kubectl -n lxcfs delete -f deployment/mutatingwebhook-ca-bundle-${LIST_TYPE}.yaml
 kubectl -n lxcfs delete -f deployment/service.yaml
 kubectl -n lxcfs delete -f deployment/deployment.yaml
 kubectl -n lxcfs delete secret lxcfs-admission-webhook-certs
